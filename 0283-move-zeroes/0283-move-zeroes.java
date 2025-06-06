ab62574caf0e0c9b8145ef1cb248e40f216119bc
Saumya Pandey <pandeysaumya1630@gmail.com>
2025-06-07 00:01:52 +0530

    Time: 2 ms (81.66%), Space: 46.2 MB (32.26%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int lastNonZeroIndex = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != lastNonZeroIndex) {
+                    int temp = nums[i];
+                    nums[i] = nums[lastNonZeroIndex];
+                    nums[lastNonZeroIndex] = temp;
+                }
+                lastNonZeroIndex++;
+            }
+        }
+    }
+}
